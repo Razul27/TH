d3bb2e319642b03069452df3a638ba56d15f2448
Razul27 <mohamedrazul916@gmail.com>
2022-07-23 15:18:51 +0530

    New Code

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,40 @@
+package org.bank;
+
+public class AxisBank {
+	public static void main(String[] args) {
+		int i=0;
+while(i<10) {
+	
+	System.out.println(i);
+	i++;
+}
+
+int j=10;
+while(j<10) {
+	System.out.println(i);
+	j++;
+	}
+	}
+}
+//int k=0;
+//while(k<10) {
+//	System.out.println(i);
+//	i++;
+//	}
+//	}
+//}
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,14 @@
+package org.bank;
+
+public class BankInfo {
+	public void saving() {
+		System.out.println("The Employee saving account balance is 20,000 Rs");
+	}
+	public void fixed() {
+		System.out.println("The Employee fixed Deposit amount is 1,00000 Rs");
+	}
+	public void deposit() {
+		System.out.println("The Employee Deposit amount is 70,000");
+	}
+
+}
